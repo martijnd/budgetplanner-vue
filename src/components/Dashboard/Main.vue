270a48ca9a0c7bf27608147654889edda9de92ca
Martijn Dorsman <mdorsman@flaire.nl>
2018-05-11 14:26:09 +0200

    wip

---
+++ src/components/Dashboard/Main.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="container">
+    <h2 class="title is-2">test</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section">
+    <Main/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from '@/components/Dashboard/Main.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Main
+  }
+}
+</script>
